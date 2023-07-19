124608d3b81fc0e458316bb30e38fa55a0b652d5
pete <kiettran255@gmail.com>
2023-07-19 15:17:18 +0700

    fix attempt

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,16 +11,6 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
-  }) as Adapter,
-  callbacks: {
-    async jwt({token}){
-      token.userRole = "admin"
-      return token;
-    }
-  }
 });
 
 export { handler as GET, handler as POST };
--- components/supabaseProvider.tsx
+++ components/supabaseProvider.tsx
@@ -26,7 +26,7 @@ const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  ) as SupabaseClient;
 
   const [user, setUser] = useState<User | null>(null);
 
