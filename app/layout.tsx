8a918b64409cc0db739890678103a13da62f4afc
pete <kiettran255@gmail.com>
2023-07-21 13:24:18 +0700

    remove boiler

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,9 @@ import Link from "next/link";
 import { SupabaseProvider } from "../components/supabaseProvider";
 import Header from "../components/Header";
 
-let title = "Dream Room Generator";
-let description = "Generate your dream room in seconds.";
-let ogimage = "https://roomgpt-demo.vercel.app/og-image.png";
-let sitename = "roomGPT.io";
+let title = "MyArchtectAI";
+let description = "MyArchtectAI";
+let ogimage = "https://myarchitectai.vercel.app/og-image.png";
 
 const inter = Inter_Tight({ subsets: ["latin"] });
 
@@ -24,17 +23,14 @@ export const metadata: Metadata = {
     icon: "/favicon.png",
   },
   openGraph: {
-    images: [ogimage],
     title,
     description,
-    url: "https://roomgpt-demo.vercel.app",
-    siteName: sitename,
+    url: "https://myarchitectai.vercel.app",
     locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    images: [ogimage],
     title,
     description,
   },
